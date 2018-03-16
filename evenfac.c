6f8750af6d4d43a364984347e8f1a169b9af273c
heerakrishnasamy <30829823+heerakrishnasamy@users.noreply.github.com>
2018-03-16 14:13:54 +0530

    Create evenfac.c

---
+++ evenfac.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  int n,i,s;
+  scanf("%d",&n);
+  for(i=n;i>=1;i--)
+  {
+   if(n%i==0)
+   {
+     s=n/i;
+   if(s%2==0)
+   {
+     printf("%d ",s);
+   }
+   }
+  }
+  return 0;
+}
